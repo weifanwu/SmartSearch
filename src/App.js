3e6f69894b907ef7805a5562afab19e85addc054
weifan <weifan@uw.edu>
2025-01-06 17:54:45 +0800

    create react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import Results from './components/Results';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <SearchBar />
-      <Results />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
